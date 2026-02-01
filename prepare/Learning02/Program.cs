98c6054ccde3fd4bcf63d0f3f9d21bb6f422f10a
Rachel Coleman <col22025@byui.edu>
2026-01-31 19:17:41 -0700

    Job class add

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
     }
 }
