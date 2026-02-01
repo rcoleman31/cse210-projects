df57af9404d1cb04b98ad303eac195ef93637355
Rachel Coleman <col22025@byui.edu>
2026-01-31 19:29:19 -0700

    Resume class add

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,10 +1,13 @@
 using System;
-using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Job job1 = new Job();
+        job1._jobTitle = "Cashier";
+        job1._company = "Target";
+        job1._startYear = 2020;
+        job1._endYear = 2025;
     }
 }
