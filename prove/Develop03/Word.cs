6eba5024fb9145594376eb6b3e77f2315d419152
Rachel Coleman <col22025@byui.edu>
2026-02-11 12:57:23 -0700

    updated code 2/11/2026 12:57

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Diagnostics;
 
-public class World
+class Word
 {
-    public string _word;
-    
+    private string _word;
+    private bool _hidden;
+
+
+    public Word(string word)
+    {
+        _word = word;
+        _hidden = false;
+    }
+
+    public void Hide()
+    {
+        
+    }
 }
