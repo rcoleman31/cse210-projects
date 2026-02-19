fb62335adb9ec284703e1c3833e1a91965c6b824
Rachel Coleman <col22025@byui.edu>
2026-02-18 19:19:24 -0700

    program finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,11 +8,24 @@ class Program
         Console.WriteLine(assignment.GetSummary());
         Console.WriteLine();
 
-        MathAssignment math = new MathAssignment(
+        Math math = new Math(
             "Roberto Rodriguez",
             "Fractions",
             "7.3",
             "8-19"
         );
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        Writing writing = new Writing(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInfo());
     }
 }
