c8a4f2bf271c017f0c75140d0195bf23106488ed
Rachel Coleman <col22025@byui.edu>
2026-02-23 13:29:51 -0700

    finished Listing class

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,1 +1,43 @@
 using System;
+
+public class Listing : Activity
+{
+    private List<string> prompts = new List<string>
+    {
+        "",
+        "",
+        ""
+    };
+
+    public Listing()
+        :base("Listing", "This activity will help you reflect on the good things in your life.")
+    {
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        Random rand = new Random();
+        Console.WriteLine("\nList as many responses as you can to the following prompt:");
+        Console.WriteLine($"---{prompts[rand.Next(prompts.Count)]}---");
+
+        Console.WriteLine("\nYou may begin in: ");
+        ShowCountdown(5);
+        Console.WriteLine();
+
+        int count = 0;
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"\nYou listed {count} items!");
+
+        DisplayEndingMessage();
+    }
+}
