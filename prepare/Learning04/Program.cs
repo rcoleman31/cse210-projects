3b018fa8508c85e40fb20781a52656c6afb48f30
Rachel Coleman <col22025@byui.edu>
2026-03-05 22:07:59 -0700

    Learning Activity 04 finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,72 @@
 using System;
 
-class Program
+public abstract class Employee
+{
+    protected string _name;
+
+    public Employee(string name)
+    {
+        _name = name;
+    }
+
+    public abstract float CalculatePay();
+
+    public void DisplayName()
+    {
+        Console.WriteLine($"Employee: {_name}");
+    }
+}
+
+public class SalaryEmployee : Employee
+{
+    private float _salary;
+
+    public SalaryEmployee(string name, float salary) : base(name)
+    {
+        _salary = salary;
+    }
+
+    public override float CalculatePay()
+    {
+        return _salary;
+    }
+}
+
+public class HourlyEmployee : Employee
+{
+    private float _rate;
+    private float _hours;
+
+    public HourlyEmployee(string name, float rate, float hours) : base(name)
+    {
+        _rate = rate;
+        _hours = hours;
+    }
+
+    public override float CalculatePay()
+    {
+        return _rate * _hours;
+    }
+}
+
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        List<Employee> employees = new List<Employee>();
+
+        employees.Add(new SalaryEmployee("Alice", 1200f));
+        employees.Add(new HourlyEmployee("Bob", 10, 40f));
+        employees.Add(new HourlyEmployee("Charlie", 18f, 35f));
+
+        foreach (Employee employee in employees)
+        {
+            employee.DisplayName();
+
+            float pay = employee.CalculatePay();
+
+            Console.WriteLine($"Paycheck Amount: {pay}");
+            Console.WriteLine();
+        }
     }
 }
