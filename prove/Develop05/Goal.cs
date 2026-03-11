cb5548d3de90b5d8e54150cae0e7d4c9a3d8baee
Rachel Coleman <col22025@byui.edu>
2026-03-11 13:00:06 -0600

    add Goal class with properties and RecordEvent method

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+    protected bool _isComplete;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+        _isComplete = false;
+    }
+
+    public virtual int RecordEvent()
+    {
+        return _points;
+    }
+}
