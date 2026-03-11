e94485eff741f3e573e4310de05e692fd226f9b4
Rachel Coleman <col22025@byui.edu>
2026-03-11 13:05:26 -0600

    added methods to Goal class

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -19,4 +19,15 @@ public class Goal
     {
         return _points;
     }
+
+    public virtual string GetDetailsString()
+    {
+        string status = _isComplete ? "[X]" : "[ ]";
+        return $"{status} {_name} ({_description})";
+    }
+
+    public virtual string GetSaveString()
+    {
+        return $"{_name}|{_description}|{_points}|{_isComplete}";
+    }
 }
