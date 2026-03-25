410a01121c6ab6956e74310046552572fd900ea2
Rachel Coleman <col22025@byui.edu>
2026-03-25 13:11:18 -0600

    Order class finished

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,2 +1,56 @@
 using System;
 
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product p in _products)
+        {
+            total += p.GetTotalCost();
+        }
+
+        // Shipping
+        if (_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string result = "Packing Label:\n";
+
+        foreach (Product p in _products)
+        {
+            result +=p.GetPackingInfo() + "\n";
+        }
+
+        return result;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
+}
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Customer 1 (USA)
+        Address a1 = new Address("123 Main St", "Rexburg", "ID", "USA");
+        Customer c1 = new Customer("Rachel Coleman", a1);
+
+        Order order1 = new Order(c1);
+        order1.AddProduct(new Product("Book", "B001", 10, 2));
+        order1.AddProduct(new Product("Pen", "P002", 2, 5));
+
+
+        // Customer 2 (International)
+        Address a2 = new Address("456 King Rd", "Toronto", "ON", "Canada");
+        Customer c2 = new Customer("Alex Smith", a2);
+
+        Order order2 = new Order(c2);
+        order2.AddProduct(new Product("Notebook", "N003", 5, 3));
+        order2.AddProduct(new Product("Pencil", "P004", 1, 10));
+
+
+        // Display Order 1
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost()}");
+        Console.WriteLine();
+
+
+        // Display Order 2
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost()}");
+        Console.WriteLine();
     }
 }
