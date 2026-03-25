f4f55afab549f98e2c2a5b8c18c722a4fb5eb97c
Rachel Coleman <col22025@byui.edu>
2026-03-25 13:46:47 -0600

    Event base class finished

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -2,5 +2,33 @@ using System;
 
 public class Event
 {
-    
+    protected string _title;
+    protected string _description;
+    protected string _date;
+    protected string _time;
+    protected Address _address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"{_title}\n{_description}\n{_date} at {_time}\n{_address.GetFullAddress()}";
+    }
+
+    public virtual string GetFullDetials()
+    {
+        return GetStandardDetails();
+    }
+
+    public string GetShortDescription()
+    {
+        return $"Event: {_title} on {_date}";
+    }
 }
