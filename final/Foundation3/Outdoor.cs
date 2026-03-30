89c9bb711cc97c96473d161d9b816b2eabf282f2
Rachel Coleman <col22025@byui.edu>
2026-03-30 13:20:28 -0600

    finished Outdoor class

--- final/Foundation3/Outdoor.cs
+++ final/Foundation3/Outdoor.cs
@@ -2,5 +2,17 @@ using System;
 
 public class Outdoor : Event
 {
-    
+    private string _weather;
+
+    public Outdoor(string title, string description, string date, string time,
+        Address address, string weather)
+        : base(title, description, date, time, address)
+    {
+        _weather = weather;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nType: Outdoor Event\nWeather: {_weather}";
+    }
 }
