1f7254a7434c6041e8bddc174e8ef8b009abed7d
Rachel Coleman <col22025@byui.edu>
2026-03-30 13:15:47 -0600

    finished Reception class

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -2,5 +2,17 @@ using System;
 
 public class Reception : Event
 {
-    
+    private string _email;
+
+    public Reception(string title, string description, string date, string time,
+        Address address, string email)
+        : base(title, description, date, time, address)
+    {
+        _email = email;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nType: Reception\nRSVP: {_email}";
+    }
 }
