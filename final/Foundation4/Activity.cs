12cfc21f7f1698555cd83ec42b5a561876099c85
Rachel Coleman <col22025@byui.edu>
2026-04-01 13:18:49 -0600

    finished Activity class

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -2,5 +2,32 @@ using System;
 
 public class Activity
 {
-    
+    protected string _date;
+    protected int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min) - Distance {GetDistance():0.0}, Speed {GetSpeed():0.0}, Pace {GetPace():0.0}";
+    }
 }
